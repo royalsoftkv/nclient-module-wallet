428c54d6c145173fb21b9e4c9635c215b8b7bb8b
royalsoftkv <38809329+royalsoftkv@users.noreply.github.com>
2021-05-31 17:46:44 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f nclient-module-wallet*.tgz
-node increase_version.js
-npm pack
-cp nclient-module-wallet-*.tgz nclient-module-wallet.tgz
-curl -F "file=@./nclient-module-wallet.tgz" http://159.69.2.203:3030/upload
